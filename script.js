07b4a7d741697cd98222ee8f9f84b96f702943b9
garbar <angelica.farmyga@gmail.com>
2026-01-16 21:53:57 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
 function generate() {
   const name = document.getElementById("name").value;
   const role = document.getElementById("role").value;
+  const date = document.getElementById("joinDate").value;
 
-  if (!name) {
-    alert("Please enter your name");
+  if (!name || !date) {
+    alert("Please enter name and join date");
     return;
   }
-
-  const date = new Date().toLocaleDateString();
-  const certId = "COMM-" + Date.now();
 
   document.getElementById("certName").innerText = name;
   document.getElementById("certRole").innerText = role;
   document.getElementById("certDate").innerText = date;
-  document.getElementById("certId").innerText = certId;
 
   document.getElementById("certificate").style.display = "block";
 }
-
