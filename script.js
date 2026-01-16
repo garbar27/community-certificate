e1acf63f0134692e3fbfafc97f4c9af60c917003
garbar <angelica.farmyga@gmail.com>
2026-01-16 20:44:36 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function generate() {
+  const name = document.getElementById("name").value;
+  const role = document.getElementById("role").value;
+
+  if (!name) {
+    alert("Please enter your name");
+    return;
+  }
+
+  const date = new Date().toLocaleDateString();
+  const certId = "COMM-" + Date.now();
+
+  document.getElementById("certName").innerText = name;
+  document.getElementById("certRole").innerText = role;
+  document.getElementById("certDate").innerText = date;
+  document.getElementById("certId").innerText = certId;
+
+  document.getElementById("certificate").style.display = "block";
+}
 
