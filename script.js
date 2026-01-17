18a692a04bc7b70332c0e5a82e183a5497b84140
garbar <angelica.farmyga@gmail.com>
2026-01-17 19:48:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function generateCertificate() {
   document.getElementById("certRole").innerText = role;
   document.getElementById("certDate").innerText = date;
 }
+
+async function downloadPDF() {
+  const { jsPDF } = window.jspdf;
+  const cert = document.getElementById("certificate");
+
+  const canvas = await html2canvas(cert, {
+    scale: 2,
+    backgroundColor: null
+  });
+
+  const imgData = canvas.toDataURL("image/png");
+  const pdf = new jsPDF("landscape", "px", [canvas.width, canvas.height]);
+
+  pdf.addImage(imgData, "PNG", 0, 0, canvas.width, canvas.height);
+  pdf.save("MagicBlock_Certificate.pdf");
+}
