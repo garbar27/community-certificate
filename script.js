d00033697a44c4904b814ec0091b6722dfd8c25c
garbar <angelica.farmyga@gmail.com>
2026-01-17 18:49:42 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function generateCertificate() {
   const name = document.getElementById("nameInput").value || "NAME";
   const role = document.getElementById("roleSelect").value;
-  const dateInput = document.getElementById("dateInput").value;
+  const date = document.getElementById("dateInput").value || "—";
 
-  document.getElementById("certName").textContent = name;
-  document.getElementById("certRole").textContent = role;
-  document.getElementById("certDate").textContent = dateInput || "—";
+  document.getElementById("certName").innerText = name;
+  document.getElementById("certRole").innerText = role;
+  document.getElementById("certDate").innerText = date;
 }
